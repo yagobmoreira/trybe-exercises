749ec4597ca191916e33e7b71c530c9f19607ca6
Yago Moreira <yagomoreira@gmail.com>
2023-05-02 19:12:04 -0300

    exercicio 05

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -1,9 +1,10 @@
-let num = .30;
+let ang1 = 30;
+let ang2 = 30;
+let ang3 = 120;
 
-if (num > 0) {
-    console.log('Positive');
-} else if (num < 0){
-    console.log('Negative');
+let somaAngulos = ang1 + ang2 + ang3;
+if (ang1 > 0 && ang2 > 0 && ang3 > 0 && somaAngulos === 180) {
+    console.log(true);
 } else {
-    console.log('Zero');
+    console.log(false);
 }
