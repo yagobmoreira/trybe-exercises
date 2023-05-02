faf18d009cee7eecbbd261ddf309a28d89e97c76
Yago Moreira <yagomoreira@gmail.com>
2023-05-02 19:33:28 -0300

    exercicio 08

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -1,17 +1,9 @@
-let porcentagem = 90;
-let conceito = "";
+let num1 = 3;
+let num2 = 4;
+let num3 = 5;
 
-if (porcentagem < 0 || porcentagem > 100) {
-  console.log("Erro");
-} else if (porcentagem >= 90) {
-  conceito = "A";
-} else if (porcentagem >= 80) {
-  conceito = "B";
-} else if (porcentagem >= 70) {
-  conceito = "C";
-} else if (porcentagem >= 60) {
-  conceito = "D";
-} else if (porcentagem < 50) {
-  conceito = "E";
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0){
+  console.log('Algum número é par.');
+} else {
+  console.log('Nenhum número é par.');
 }
-console.log(conceito);
