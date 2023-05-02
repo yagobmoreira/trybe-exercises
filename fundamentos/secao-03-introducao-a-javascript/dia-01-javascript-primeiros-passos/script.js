7f26aea2f09d2d2c5e81887bc5ce334c3ca6ff64
Yago Moreira <yagomoreira@gmail.com>
2023-05-02 19:07:17 -0300

    exercicio 03

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -1,10 +1,11 @@
 let num1 = 20;
 let num2 = 30;
+let num3 = 112;
 
-if (num1 > num2) {
-    console.log('Numéro 1 é maior que o número 2');
-} else if (num2 > num1 ){
-    console.log('Número 2 é maior que o número 1');
-} else {
-    console.log('Os números são iguais');
+if (num1 > num2 && num1 > num3) {
+    console.log('Numéro 1 é o maior');
+} else if (num2 > num1 && num2 > num3){
+    console.log('Número 2 é o maior');
+} else if (num3 > num1 && num3 > num2){
+    console.log('Número 3 é o maior');
 }
