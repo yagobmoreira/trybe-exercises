7da10406d90ae7ca78322ecb753ba7de693bf229
Yago Moreira <yagomoreira@gmail.com>
2023-05-02 19:05:18 -0300

    exercicio 02

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -0,0 +1,10 @@
+let num1 = 20;
+let num2 = 30;
+
+if (num1 > num2) {
+    console.log('Numéro 1 é maior que o número 2');
+} else if (num2 > num1 ){
+    console.log('Número 2 é maior que o número 1');
+} else {
+    console.log('Os números são iguais');
+}
