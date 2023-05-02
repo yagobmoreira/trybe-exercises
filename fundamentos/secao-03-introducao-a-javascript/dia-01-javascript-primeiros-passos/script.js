15f6877b86391b073c439fee959240292c3604bf
Yago Moreira <yagomoreira@gmail.com>
2023-05-02 19:30:36 -0300

    exercicio 07

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -1,27 +1,17 @@
-let peca = "BISHOP";
-let result = peca.toLowerCase();
+let porcentagem = 90;
+let conceito = "";
 
-switch (result) {
-  case "bishop":
-    console.log(result + " moves diagonally");
-    break;
-  case "king":
-    console.log(result + " moves from its square to a neighboring square");
-    break;
-  case "rook":
-    console.log(result + " can move in its line or row");
-    break;
-  case "queen":
-    console.log(result + " may move like a Rook or a Bishop");
-    break;
-  case "knight":
-    console.log(result + " jumps in making the shortest move that is not a straight one"
-    );
-    break;
-  case "pawn":
-    console.log(result + " moves one square straight ahead");
-    break;
-  default:
-    console.log('Error')
-    break;
+if (porcentagem < 0 || porcentagem > 100) {
+  console.log("Erro");
+} else if (porcentagem >= 90) {
+  conceito = "A";
+} else if (porcentagem >= 80) {
+  conceito = "B";
+} else if (porcentagem >= 70) {
+  conceito = "C";
+} else if (porcentagem >= 60) {
+  conceito = "D";
+} else if (porcentagem < 50) {
+  conceito = "E";
 }
+console.log(conceito);
