3309b626bc1e9c813e24b8a2a3850d1befb65e4c
Yago Moreira <yagomoreira@gmail.com>
2023-05-02 19:56:41 -0300

    exercicio 11

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -1,13 +1,28 @@
-let valorCusto = 80;
-let impostoCusto = 0.2*valorCusto;
-let valorCustoTotal = valorCusto + impostoCusto;
-let valorVenda = 140;
-let lucro = valorVenda - valorCustoTotal;
-let numVendas = 1000;
+let salarioBruto = 3000;
+let aliquotaInss;
+let aliquotaIr;
 
-if (valorCusto >= 0){
-  lucroTotal = lucro*numVendas;
+if (salarioBruto <= 1556.94){
+  aliquotaInss = salarioBruto * 0.8;
+} else if (salarioBruto <= 2594.92){
+  aliquotaInss = salarioBruto * 0.9;
+} else if (salarioBruto <= 5189.82){
+  aliquotaInss = salarioBruto * 0.11;
 } else {
-  console.log('Erro');
+  aliquotaInss = 570.88;
 }
-console.log(lucroTotal);
+
+let salarioBase = salarioBruto - aliquotaInss;
+
+if (salarioBase >= 1903.99 && salarioBase <= 2826.65){
+  aliquotaIr = (salarioBase * 0.075) - 142.80;
+} else if (salarioBase >= 2826.66 && salarioBase <= 3751.05){
+  aliquotaIr = (salarioBase * 0.015) - 354.80;
+} else if (salarioBase >= 3751.06 && salarioBase < 4664.68){
+  aliquotaIr = (salarioBase * 0.0225) - 636.13;
+} else if (salarioBase >= 4664.69){
+  aliquotaIr = (salarioBase * 0.0275) - 869.36;
+}
+console.log(aliquotaIr);
+let salarioLiquido = salarioBase - aliquotaIr;
+console.log(salarioLiquido);
