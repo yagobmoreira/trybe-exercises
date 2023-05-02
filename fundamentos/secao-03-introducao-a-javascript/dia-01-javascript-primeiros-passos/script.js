c15de6bb433422b7d3f18d7d9135b38873b4c2c2
Yago Moreira <yagomoreira@gmail.com>
2023-05-02 19:23:49 -0300

    exercicio 06

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/script.js
@@ -1,10 +1,27 @@
-let ang1 = 30;
-let ang2 = 30;
-let ang3 = 120;
+let peca = "BISHOP";
+let result = peca.toLowerCase();
 
-let somaAngulos = ang1 + ang2 + ang3;
-if (ang1 > 0 && ang2 > 0 && ang3 > 0 && somaAngulos === 180) {
-    console.log(true);
-} else {
-    console.log(false);
+switch (result) {
+  case "bishop":
+    console.log(result + " moves diagonally");
+    break;
+  case "king":
+    console.log(result + " moves from its square to a neighboring square");
+    break;
+  case "rook":
+    console.log(result + " can move in its line or row");
+    break;
+  case "queen":
+    console.log(result + " may move like a Rook or a Bishop");
+    break;
+  case "knight":
+    console.log(result + " jumps in making the shortest move that is not a straight one"
+    );
+    break;
+  case "pawn":
+    console.log(result + " moves one square straight ahead");
+    break;
+  default:
+    console.log('Error')
+    break;
 }
