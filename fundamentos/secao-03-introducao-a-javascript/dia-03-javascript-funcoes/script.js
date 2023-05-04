cee88ddd9690427cd2a96c217eb5ccb7c5bfcd92
Yago Moreira <yagomoreira@gmail.com>
2023-05-04 15:42:07 -0300

    exemplo 02

--- fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/script.js
@@ -12,4 +12,16 @@ function addCliente(nome) {
 
 console.log(addCliente('Yago'));
 
+// Agora iremos escrever um programa que irá excluir um(a) cliente já existente do array de clientes do TrybeBank. Certifique-se de que a função deve receber um parâmetro do tipo string e imprimir uma mensagem de erro, caso o parâmetro não seja do tipo string e caso o(a) cliente não exista dentro do array.
 
+function removeCliente(nome) {
+  if (typeof(nome) === 'string' && clientesTrybeBank.indexOf(nome) >= 0){
+    clientesTrybeBank.splice(clientesTrybeBank.indexOf(nome),1);
+    return clientesTrybeBank;
+  } else {
+    return 'Erro: parâmetro inserido não é do tipo string.'
+  }
+}
+
+console.log(clientesTrybeBank);
+console.log(removeCliente('John'));
