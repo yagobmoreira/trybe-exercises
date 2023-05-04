4f5da2df42d8e718df309ef77479f1c8fc6bea30
Yago Moreira <yagomoreira@gmail.com>
2023-05-04 15:59:45 -0300

    nova função para diminuir complexidade de removeCliente

--- fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/script.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-03-javascript-funcoes/script.js
@@ -1,27 +1,36 @@
 // Faça um programa para adicionar clientes ao array de clientes do TrybeBank. Certifique-se de que a função deve receber um parâmetro do tipo string e imprimir uma mensagem de erro caso o parâmetro não seja do tipo string.
-let clientesTrybeBank = ['Ada', 'John', 'Gus'];
+let clientesTrybeBank = ["Ada", "John", "Gus"];
 
-function addCliente(nome) { 
-  if (typeof(nome) === 'string'){
+function addCliente(nome) {
+  if (typeof nome === "string") {
     clientesTrybeBank.push(nome);
     return clientesTrybeBank;
   } else {
-    return 'Erro: parâmetro inserido não é do tipo string.'
+    return "Erro: parâmetro inserido não é do tipo string.";
   }
 }
 
-console.log(addCliente('Yago'));
+console.log(addCliente("Yago"));
 
 // Agora iremos escrever um programa que irá excluir um(a) cliente já existente do array de clientes do TrybeBank. Certifique-se de que a função deve receber um parâmetro do tipo string e imprimir uma mensagem de erro, caso o parâmetro não seja do tipo string e caso o(a) cliente não exista dentro do array.
 
+function verificaCliente(nome) {
+  let clienteValido = false;
+  if (typeof nome === "string" && clientesTrybeBank.indexOf(nome) >= 0) {
+    clienteValido = true;
+  }
+  return clienteValido;
+}
+
 function removeCliente(nome) {
-  if (typeof(nome) === 'string' && clientesTrybeBank.indexOf(nome) >= 0){
-    clientesTrybeBank.splice(clientesTrybeBank.indexOf(nome),1);
+  let clienteExistente = verificaCliente(nome);
+  if (clienteExistente) {
+    clientesTrybeBank.splice(clientesTrybeBank.indexOf(nome), 1);
     return clientesTrybeBank;
   } else {
-    return 'Erro: parâmetro inserido não é do tipo string.'
+    return "Erro: parâmetro inserido não é do tipo string.";
   }
 }
 
 console.log(clientesTrybeBank);
-console.log(removeCliente('John'));
+console.log(removeCliente("Gus"));
