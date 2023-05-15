bcb43b44628d1f53b68c1b08db1db5dd5ac9b089
Yago Moreira <yagomoreira@gmail.com>
2023-05-15 19:06:57 -0300

    finalizando exemplos

--- fundamentos/secao-04-javascript-DOM-eventos-webstorage/dia-03-javascript-eventos/main.js
+++ fundamentos/secao-04-javascript-DOM-eventos-webstorage/dia-03-javascript-eventos/main.js
@@ -20,22 +20,33 @@ firstLi.addEventListener('click', addClass);
 secondLi.addEventListener('click', addClass);
 thirdLi.addEventListener('click', addClass);
 
-  
-
-
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech'.
-
+input.addEventListener('input', (evento) => {
+  const techElement = document.querySelector('.tech');
+  // console.log(event.target.value);
+  techElement.innerText = evento.target.value;
+})
+  
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy',
 // redirecione para alguma página.
 // 4.1. Que tal redirecionar para seu portfólio? Dica: Lembre-se dos métodos `window.location.replace` e `window.open`.
+myWebpage.addEventListener('dblclick',(evento) => {
+  window.location.replace('https://yagobmoreira.github.io/')
+})
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo.
-
+const changeColor = () => {
+  myWebpage.addEventListener('mouseout', (evento) => {
+    console.log(evento.target);
+    evento.target.style.color = 'blue';
+  })
+}
+changeColor();
 // Segue abaixo um exemplo de uso do event.target.
 
-const resetText = (event) => {
+function resetText(event) {
   // O event é passado como um parâmetro para a função.
   event.target.innerText = 'Opção reiniciada';
   // O event possui várias propriedades, porém a mais usada é o event.target,
