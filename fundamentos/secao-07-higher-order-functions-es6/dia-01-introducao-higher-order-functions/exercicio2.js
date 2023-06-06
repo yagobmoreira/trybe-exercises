709e961a9adde1fdb54ea2009b165ea7a02cc9b5
Yago Moreira <yagomoreira@gmail.com>
2023-06-06 11:55:59 -0300

    Resolvendo exercicio bonus

--- fundamentos/secao-07-higher-order-functions-es6/dia-01-introducao-higher-order-functions/exercicio2.js
+++ fundamentos/secao-07-higher-order-functions-es6/dia-01-introducao-higher-order-functions/exercicio2.js
@@ -1,12 +1,10 @@
-const { books } = require("./data");
+const { books } = require('./data');
 
 //Organizando uma biblioteca
 
 // Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
 
-const birthYear1947 = books.find(
-  (element) => element.author.birthYear === 1947
-);
+const birthYear1947 = books.find((element) => element.author.birthYear === 1947); 
 const authorName = birthYear1947.author.name;
 // console.log(authorName);
 
@@ -15,21 +13,21 @@ const authorName = birthYear1947.author.name;
 const smallerName = (books) => {
   let bookNameLength = books[0].name.length;
   let indiceDoMenor = 0;
-  books.forEach((book, index) => {
-    if (book.name.length < bookNameLength) {
-      bookNameLength = book.name.length;
-      indiceDoMenor = index;
-    }
-  });
+    books.forEach((book, index) => {
+      if (book.name.length < bookNameLength) {
+        bookNameLength = book.name.length;
+        indiceDoMenor = index;
+      }
+    });
   return `O nome do livro com menor número de caracteres é: ${books[indiceDoMenor].name}`;
-};
-console.log(smallerName(books));
+}
+console.log(smallerName(books))
 
 // Encontre o primeiro livro cujo nome possua 26 caracteres.
 
 const livro = (books) => {
   return books.find((element) => element.name.length === 26);
-};
+}
 
 console.log(livro(books));
 
@@ -38,9 +36,7 @@ console.log(livro(books));
 // const expectedResult = false;
 
 function everyoneWasBornOnSecXX() {
-  return books.every(
-    (book) => book.author.birthYear > 1900 && book.author.birthYear < 2001
-  );
+  return books.every((book) => book.author.birthYear > 1900 && book.author.birthYear < 2001)
 }
 
 // Faça uma função que retorne true, se algum livro foi lançado na década de 80, e false, caso contrário.
@@ -48,10 +44,8 @@ function everyoneWasBornOnSecXX() {
 // const expectedResult = true;
 
 const someBookWasReleaseOnThe80s = () => {
-  return books.some(
-    (book) => book.releaseYear >= 1980 && book.releaseYear <= 1989
-  );
-};
+  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear <= 1989)
+}
 // console.log(someBookWasReleaseOnThe80s());
 
 // Faça uma função que retorne true, caso nenhuma pessoa autora tenha nascido no mesmo ano, e false, caso contrário.
