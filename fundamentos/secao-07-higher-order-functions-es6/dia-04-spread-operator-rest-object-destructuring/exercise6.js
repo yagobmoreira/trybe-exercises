2754c1d809287b8880c1f5852b5e732c548c4a48
Yago Moreira <yagomoreira@gmail.com>
2023-06-12 18:32:36 -0300

    Exercise 6

---
+++ fundamentos/secao-07-higher-order-functions-es6/dia-04-spread-operator-rest-object-destructuring/exercise6.js
@@ -0,0 +1,18 @@
+
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
+// yearSeasons: estações do ano.
+
+const {
+  spring,
+  summer,
+  autumn,
+  winter,
+} = yearSeasons;
+
+const months = [...spring, ...summer, ...autumn, ...winter];
+console.log(months);
