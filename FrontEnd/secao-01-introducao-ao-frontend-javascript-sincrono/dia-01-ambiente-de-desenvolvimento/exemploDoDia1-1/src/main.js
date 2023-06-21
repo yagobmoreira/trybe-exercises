95d7f65ba2a038e65eb9fe41f7f6ae713ab50322
Yago Moreira <yagomoreira@gmail.com>
2023-06-21 18:55:20 -0300

    Exercício de fixação

--- FrontEnd/secao-01-introducao-ao-frontend-javascript-sincrono/dia-01-ambiente-de-desenvolvimento/exemploDoDia1-1/src/main.js
+++ FrontEnd/secao-01-introducao-ao-frontend-javascript-sincrono/dia-01-ambiente-de-desenvolvimento/exemploDoDia1-1/src/main.js
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid';
 import './style.css';
+import copy from 'clipboard-copy';
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
@@ -7,4 +8,10 @@ const displayPasswordEl = document.querySelector('h2');
 passwordBtnEl.addEventListener('click', () => {
   const randomPassword = nanoid();
   displayPasswordEl.innerHTML = randomPassword;
+});
+
+displayPasswordEl.addEventListener('click', (event) => {
+  copy(event.target.innerHTML);
+  // eslint-disable-next-line no-alert
+  alert('Senha copiada');
 });
