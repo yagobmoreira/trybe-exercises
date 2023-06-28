8c261dacea0ea5563495cbc7da8e2088ebce8616
Yago Moreira <yagomoreira@gmail.com>
2023-06-28 11:23:04 -0300

    finalizando conteudo do dia 2.1

--- FrontEnd/secao-02-introducao-ao-react/dia-01-introducao-ao-react-e-typescript/my-react-app/src/App.tsx
+++ FrontEnd/secao-02-introducao-ao-react/dia-01-introducao-ao-react-e-typescript/my-react-app/src/App.tsx
@@ -1,10 +1,13 @@
-import Header from './components/header';
+// import Header from './components/header';
 import Footer from './components/footer';
+import Greeting from './components/greeting';
+import LuckyNumbers from './components/luckyNumbers';
 
 function App() {
   return (
     <div>
-      <Header />
+      <Greeting />
+      <LuckyNumbers />
       <Footer />
     </div>
   );
