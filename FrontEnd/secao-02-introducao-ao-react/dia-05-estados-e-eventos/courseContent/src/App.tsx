199924b20c1a2bad7f9e6f3131606273fdf61b2e
Yago Moreira <yagomoreira@gmail.com>
2023-07-06 19:00:38 -0300

    Botao de adicionar na lista

--- FrontEnd/secao-02-introducao-ao-react/dia-05-estados-e-eventos/courseContent/src/App.tsx
+++ FrontEnd/secao-02-introducao-ao-react/dia-05-estados-e-eventos/courseContent/src/App.tsx
@@ -5,22 +5,6 @@ import { useState } from 'react';
 
 function App() {
 
-  function handleNextClick() {
-    if (index + 1 < toolKit.length) {
-      setIndex(index + 1);
-    } else {
-      setIndex(0); // Volta para o primeiro elemento
-    }
-  }
-
-  function handlePreviousClick() {
-    if (index - 1 >= 0) {
-      setIndex(index - 1);
-    } else {
-      setIndex(toolKit.length - 1); // Vai para o último elemento
-    }
-  }
-
   const toolKit = [
     'JavaScript',
     'TypeScript',
@@ -32,13 +16,51 @@ function App() {
   ];
 
   const [index, setIndex] = useState(0);
+  const [toolList, setToolList] = useState(toolKit);
+  const [inputValue, setInputValue] = useState('');
+
+  function handleNextClick() {
+    if (index + 1 < toolList.length) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0); // Volta para o primeiro elemento
+    }
+  }
+
+  function handlePreviousClick() {
+    if (index - 1 >= 0) {
+      setIndex(index - 1);
+    } else {
+      setIndex(toolList.length - 1); // Vai para o último elemento
+    }
+  }
+
+  function handleAddClick() {
+    if (inputValue) {
+      setToolList([...toolList, inputValue]);
+    }
+  }
 
   return (
     <>
       <h1>Caixa de ferramentas de uma Pessoa Desenvolvedora</h1>
-      <h2>{toolKit[index]}</h2>
+      <h2>{toolList[index]}</h2>
       <button onClick={handlePreviousClick}>Anterior</button>
       <button onClick={handleNextClick}>Próximo</button>
+      <section>
+        <h3>Adicione novas ferramentas:</h3>
+        <input onChange={({ target }) => setInputValue(target.value)} />
+        <button onClick={handleAddClick}>Adicionar</button>
+      </section>
+
+      <section>
+        <h3>Lista de itens do carrossel:</h3>
+        <ul>
+          {toolList.map((tool) => (
+            <li>{tool}</li>
+          ))}
+        </ul>
+      </section>
     </>
   )
 }
