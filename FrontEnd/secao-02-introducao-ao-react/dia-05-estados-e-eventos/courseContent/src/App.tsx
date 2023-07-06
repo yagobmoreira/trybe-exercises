b832a143dc0c8f2ef40f105bf90b7efe0ef267fd
Yago Moreira <yagomoreira@gmail.com>
2023-07-06 16:11:17 -0300

    Utilização de estados com React useState

--- FrontEnd/secao-02-introducao-ao-react/dia-05-estados-e-eventos/courseContent/src/App.tsx
+++ FrontEnd/secao-02-introducao-ao-react/dia-05-estados-e-eventos/courseContent/src/App.tsx
@@ -1,11 +1,21 @@
 import Button from './components/Button'
 import Input from './components/Input'
+import { useState } from 'react';
 // import './App.css'
 
 function App() {
+  const [showImg, setShowImg] = useState(true);
+
+  function handleClick() {
+    setShowImg(!showImg);
+  }
 
   return (
     <>
+        {showImg && <img src="/src/assets/react.svg" />}
+      <button onClick={handleClick}>
+        {showImg ? 'Esconder imagem' : 'Mostrar imagem'}
+      </button>
       <Input />
       <Button />
     </>
