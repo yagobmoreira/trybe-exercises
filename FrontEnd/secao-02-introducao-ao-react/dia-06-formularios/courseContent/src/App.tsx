f350890e19987b75b03e43d09ce17a8f803bc595
Yago Moreira <yagomoreira@gmail.com>
2023-07-07 15:45:47 -0300

    Praticando formulários

--- FrontEnd/secao-02-introducao-ao-react/dia-06-formularios/courseContent/src/App.tsx
+++ FrontEnd/secao-02-introducao-ao-react/dia-06-formularios/courseContent/src/App.tsx
@@ -1,17 +1,60 @@
 import { useState } from "react";
-import './App.css'
+import "./App.css";
 
 function App() {
-const [name, setName] = useState<string>('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [schooling, setSchooling] = useState("Médio");
+  const [resume, setResume] = useState("");
+
+  function resetForm() {
+    setName('');
+    setEmail('');
+    setSchooling('');
+    setResume('');
+  }
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    alert(
+      `Nome: ${name}\nemail: ${email}\nEscolaridade: ${schooling}\nExperiências: ${resume}`
+    );
+    resetForm();
+  }
   return (
-    <>
+
+    <form onSubmit={(event) => handleSubmit(event)}>
       <label>
-        Nome:
-        <input value={name} onChange={({target}) => setName(target.value)}/>
+        Nome
+        <input value={name} onChange={({ target }) => setName(target.value)} />
       </label>
-      <h1>Olá, {name} !</h1>
-      <button onClick={ () => setName('') }>Apagar!</button>
-    </>
+      <label>
+        E-mail
+        <input
+          value={email}
+          onChange={({ target }) => setEmail(target.value)}
+        />
+      </label>
+      <label>
+        Escolaridade
+        <select
+          value={schooling}
+          onChange={({ target }) => setSchooling(target.value)}
+        >
+          <option value="Médio">Médio</option>
+          <option value="Superior">Superior</option>
+          <option value="Pós-graduação">Pós-graduação</option>
+        </select>
+      </label>
+      <label>
+        Resumo das experiências
+        <textarea
+          value={resume}
+          onChange={({ target }) => setResume(target.value)}
+        />
+      </label>
+      <button>Enviar</button>
+    </form>
   );
 }
 
