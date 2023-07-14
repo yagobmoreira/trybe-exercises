eb04a337547ce059e8806c8bacab12827c25f624
Yago Moreira <yagomoreira@gmail.com>
2023-07-14 18:27:13 -0300

    useEffect na pratica

--- FrontEnd/secao-03-aprofundando-no-react/dia-02-useEffect/ISSlocationAPI/src/App.tsx
+++ FrontEnd/secao-03-aprofundando-no-react/dia-02-useEffect/ISSlocationAPI/src/App.tsx
@@ -1,8 +1,30 @@
+import { useState, useEffect } from "react"
+import { fetchCoordinates } from "./services";
+
+type Coordinates = {
+  latitude: number;
+  longitude: number;
+}
 
 function App() {
-  
+  const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchCoordinates();
+      setCoordinates({ latitude: data.latitude, longitude: data.longitude });
+    }
+    fetchData();
+  }, [])
   return (
     <>
+      <h1>International Space Station Location Tracker</h1>
+      {coordinates && (
+        <>
+          <h2>{`Latitude: ${coordinates.latitude}`}</h2>
+          <h2>{`Longitude: ${coordinates.longitude}`}</h2>
+        </>
+    )}
     </>
   )
 }
