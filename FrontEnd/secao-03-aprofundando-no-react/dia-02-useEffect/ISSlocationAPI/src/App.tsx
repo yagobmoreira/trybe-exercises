38a85d75b4a7219cc7d9b863e8723b3191014249
Yago Moreira <yagomoreira@gmail.com>
2023-07-14 21:02:03 -0300

    cleanup e loading

--- FrontEnd/secao-03-aprofundando-no-react/dia-02-useEffect/ISSlocationAPI/src/App.tsx
+++ FrontEnd/secao-03-aprofundando-no-react/dia-02-useEffect/ISSlocationAPI/src/App.tsx
@@ -8,14 +8,29 @@ type Coordinates = {
 
 function App() {
   const [coordinates, setCoordinates] = useState<Coordinates | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       const data = await fetchCoordinates();
       setCoordinates({ latitude: data.latitude, longitude: data.longitude });
+      setLoading(false);
     }
     fetchData();
+
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 3000);
+
+    return () => { // Essa é a função cleanup
+      clearInterval(intervalId);
+    };
   }, [])
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <>
       <h1>International Space Station Location Tracker</h1>
