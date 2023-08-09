18e92f568ee7f1fe621fdd72cb3ff302728da1ee
Yago Moreira <yagomoreira@gmail.com>
2023-08-09 14:11:41 -0300

    fn renderWithRouter e testes com mudança de rotas

--- FrontEnd/secao-04-testes-automatizados-com-RTL/dia-03-testando-com-react-router/tests-react-router/src/App.test.tsx
+++ FrontEnd/secao-04-testes-automatizados-com-RTL/dia-03-testando-com-react-router/tests-react-router/src/App.test.tsx
@@ -1,26 +1,44 @@
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import { renderWithRouter } from './helpers/renderWithRouter';
 
 import App from './App';
 
 it('Renderiza texto da página inicial', () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
+  // render(
+  //   <BrowserRouter>
+  //     <App />
+  //   </BrowserRouter>
+  // );
   // render(<App />, {wrapper: BrowserRouter}}); outra forma de usar o BrowserRouter
+  renderWithRouter(<App />);
   expect(screen.getByText(/Você está na página Início/i)).toBeInTheDocument();
 })
 
 it('Navega para página About', async () => {
-  render(<App />, { wrapper: BrowserRouter });
+  // render(<App />, { wrapper: BrowserRouter });
+  const {user} = renderWithRouter(<App />);
   expect(screen.getByText(/Você está na página Início/i)).toBeInTheDocument();
 
   const aboutLink = screen.getByRole('link', { name: /Sobre/i});
-  await userEvent.click(aboutLink);
+  await user.click(aboutLink);
   expect(screen.getByText(/Você está na página Sobre/i)).toBeInTheDocument();
 })
 
+it('Navega da página About para a página inicial', async () => {
+  const {user} = renderWithRouter(<App />, { route: '/about' });
+
+  expect(screen.getByText(/Você está na página Sobre/i)).toBeInTheDocument();
+  const homeLink = screen.getByRole('link', { name: /Início/i });
+  await user.click(homeLink);
+  expect(screen.getByText(/Você está na página Início/i)).toBeInTheDocument();
+})
+
+it('Navega para página NotFound', async () => {
+  const {user} = renderWithRouter(<App />, { route: '/pagina-nao-existente' });
+
+  expect(screen.getByText(/Página não encontrada/i)).toBeInTheDocument();
+  const homeLink = screen.getByRole('link', { name: /to-home/i });
+  await user.click(homeLink);
+  expect(screen.getByText(/Você está na página Início/i)).toBeInTheDocument();
+});
 
--- FrontEnd/secao-04-testes-automatizados-com-RTL/dia-03-testando-com-react-router/tests-react-router/src/pages/NotFound/NotFound.tsx
+++ FrontEnd/secao-04-testes-automatizados-com-RTL/dia-03-testando-com-react-router/tests-react-router/src/pages/NotFound/NotFound.tsx
@@ -1,6 +1,11 @@
+import {Link} from 'react-router-dom';
+
 function NotFound() {
   return (
-    <h1>Página não encontrada</h1>
+    <>
+      <h1>Página não encontrada</h1>
+      <Link aria-label='to-home' to="/">Voltar para a página inicial</Link>
+    </>
   );
 }
 
