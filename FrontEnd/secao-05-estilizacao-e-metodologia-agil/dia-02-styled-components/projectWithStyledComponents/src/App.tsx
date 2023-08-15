a1370c7f1914e25ea15cb0db3ef13f726307b3ed
Yago Moreira <yagomoreira@gmail.com>
2023-08-15 15:11:46 -0300

    Exportando estilos para o arquivo styles.tsx

--- FrontEnd/secao-05-estilizacao-e-metodologia-agil/dia-02-styled-components/projectWithStyledComponents/src/App.tsx
+++ FrontEnd/secao-05-estilizacao-e-metodologia-agil/dia-02-styled-components/projectWithStyledComponents/src/App.tsx
@@ -1,21 +1,6 @@
-import styled from 'styled-components'
+import {Header, Container} from './styles'
 
 function App() {
-
-  const Header = styled.header`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 1em;
-    background-color: #022c16;
-    margin-bottom: 3em;
-    `;
-
-  const Container = styled.div`
-    width: 80%;
-    max-width: 800px;
-    margin: 0 auto;
-    `;
   return (
     <>
       <Header>
