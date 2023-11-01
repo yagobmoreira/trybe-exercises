f7e14ff6594dacda89fb5333dda9546c85aa9ca3
Yago Moreira <yagomoreira@gmail.com>
2023-11-01 08:37:28 -0300

    Exercicio 6.6

--- BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodeJs-runtime-assincrono/exercises/simpsons.js
+++ BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodeJs-runtime-assincrono/exercises/simpsons.js
@@ -55,7 +55,20 @@ const addNewCharacter = async () => {
   }
 }
 
+async function replaceNelson() {
+  try {
+    const fileContent = await fs.readFile('./data/simpsonsFamily.json', 'utf-8');
+    const simpsons = JSON.parse(fileContent);
+    const simpsonsWithoutNelson = simpsons.filter((simpson) => simpson.id !== '5');
+    const simpsonsWithMaggie = simpsonsWithoutNelson
+      .concat([{ id: '15', name: 'Maggie Simpson' }]);
+    return fs.writeFile('./data/simpsonsFamily.json', JSON.stringify(simpsonsWithMaggie));
+  } catch (error) { 
+    console.error(error);
+  }
+}
+
 const main = async () => {
-  await addNewCharacter();
+  await replaceNelson();
 }
 main();
