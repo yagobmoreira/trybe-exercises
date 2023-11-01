e313ba7471da2d4cbbe6aba41e2a3d3e4b41730f
Yago Moreira <yagomoreira@gmail.com>
2023-11-01 08:21:29 -0300

    Exercicio 6.2

--- BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodeJs-runtime-assincrono/exercises/simpsons.js
+++ BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodeJs-runtime-assincrono/exercises/simpsons.js
@@ -1,4 +1,5 @@
 const data = require('./data/simpsons.json');
+const fs = require('fs').promises;
 
 const readCharacters = () => {
   for (character in data) {
@@ -6,4 +7,20 @@ const readCharacters = () => {
   }
 }
 
-readCharacters();
+const readCharacterById = async (id) => {
+  try {
+    const data = await fs.readFile('./data/simpsons.json', 'utf8');
+    const characters = JSON.parse(data);
+    const character = characters.find((character) => character.id === id);
+    if (!character) throw new Error('id não encontrado');
+    return character;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const main = async () => {
+  const simpson = await readCharacterById('2');
+  console.log(simpson);
+}
+main();
