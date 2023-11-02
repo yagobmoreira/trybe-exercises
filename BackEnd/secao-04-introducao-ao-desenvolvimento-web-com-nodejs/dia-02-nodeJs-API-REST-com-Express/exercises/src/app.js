2d1464cfade44692e151bf86820ee08cf9a8187d
Yago Moreira <yagomoreira@gmail.com>
2023-11-02 09:51:29 -0300

    Exercício 6 e 7

--- BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-nodeJs-API-REST-com-Express/exercises/src/app.js
+++ BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-02-nodeJs-API-REST-com-Express/exercises/src/app.js
@@ -7,13 +7,27 @@ app.use(express.json());
 
 app.get('/movies/:id', async (req, res) => {
   const { id } = req.params;
-  const data = await readData();
-  const findTeam = data.find((film) => film.id === Number(id));
+  const movies = await readData();
+  const findTeam = movies.find((film) => film.id === Number(id));
   if (!findTeam) {
     return res.status(404).json({ message: 'Filme não encontrado' });
   }
   return res.status(200).json({ findTeam });
 });
+
+app.get('/movies', async (req, res) => {
+  const movies = await readData();
+  return res.status(200).json({ movies });
+});
+
+app.post('/movies', async (req, res) => {
+  const { movie, price } = req.body;
+  const movies = await readData();
+  const id = movies.length + 1;
+  const newMovie = { id, movie, price };
+  movies.push(newMovie);
+  return res.status(201).json({ newMovie });
+})
 
 
 module.exports = app;
