279444bbce6a177a195857d1675efe63dc245878
Yago Moreira <yagomoreira@gmail.com>
2023-11-07 15:35:43 -0300

    Middleware express-rate-limit

--- BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodeJs-Express-e-Middlewares/soccer-team-manager/src/app.js
+++ BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodeJs-Express-e-Middlewares/soccer-team-manager/src/app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 require('express-async-errors');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const existingId = require('./middlewares/existingId');
 const validateTeam = require('./middlewares/validateTeam');
@@ -11,17 +12,21 @@ const teams = require('./utils/teams');
 
 const app = express();
 
+// Configuramos um limitador para uma taxa máxima de 100 requisições em um intervalo de 15 minutos
+const limiter = rateLimit({
+   max: 100, // número máximo de requisições
+   windowMs: 15 * 60 * 1000, // intervalo de tempo, em milissegundos, para atingir o número máximo de requisições
+   message: 'Muitas requisições originadas desta IP', // mensagem personalizada quando atinge o limit rate
+});
+
 app.use(express.json());
 app.use(express.static('./images'));
 app.use(morgan('dev'));
+app.use(limiter);
 
 app.use(apiCredentials);
 
 let nextId = 3;
-
-app.get('/brasao/fla.png', (req, res) => {
-  res.status(200);
-});
 
 app.get('/teams', (req, res) => res.json(teams));
 
