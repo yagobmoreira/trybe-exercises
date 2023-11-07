92793c74a6924a022157c8842bfd99519946adb5
Yago Moreira <yagomoreira@gmail.com>
2023-11-07 19:03:18 -0300

    Exercicio 1 - Parte 6, 7, 8 e 9

--- BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodeJs-Express-e-Middlewares/exercises/src/app.js
+++ BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodeJs-Express-e-Middlewares/exercises/src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const validateName = require('./middlewares/validateName');
 const validatePrice = require('./middlewares/validatePrice');
+const validateDescription = require('./middlewares/validateDescription');
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/activities', validateName, validatePrice,  (req, res) => {
+app.post('/activities',
+  validateName,
+  validatePrice,
+  validateDescription,
+  (req, res) => {
   res.status(201).json({message: "Atividade cadastrada com sucesso!"});
 })
 
