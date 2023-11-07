104c425c5839e45a01d610a2906167bb2d96b59e
Yago Moreira <yagomoreira@gmail.com>
2023-11-07 20:02:46 -0300

    Exercicio 2 - Parte 2

--- BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodeJs-Express-e-Middlewares/exercises/src/app.js
+++ BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodeJs-Express-e-Middlewares/exercises/src/app.js
@@ -6,12 +6,14 @@ const validateCreatedAt = require('./middlewares/validateCreatedAt');
 const validateRating = require('./middlewares/validateRating');
 const validateDifficulty = require('./middlewares/validateDifficulty');
 const generateToken = require('./utils/generateToken');
+const auth = require('./middlewares/auth');
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/activities',
+  auth,
   validateDifficulty,
   validateRating,
   validateCreatedAt,
