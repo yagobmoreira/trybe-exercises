a646dd904139203457751b33f926711af5657e79
Yago Moreira <yagomoreira@gmail.com>
2023-11-07 19:15:37 -0300

    Exercicio 1 - Parte 8 e 9

--- BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodeJs-Express-e-Middlewares/exercises/src/app.js
+++ BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-04-nodeJs-Express-e-Middlewares/exercises/src/app.js
@@ -3,12 +3,16 @@ const validateName = require('./middlewares/validateName');
 const validatePrice = require('./middlewares/validatePrice');
 const validateDescription = require('./middlewares/validateDescription');
 const validateCreatedAt = require('./middlewares/validateCreatedAt');
+const validateRating = require('./middlewares/validateRating');
+const validateDifficulty = require('./middlewares/validateDifficulty');
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/activities',
+  validateDifficulty,
+  validateRating,
   validateCreatedAt,
   validateDescription,
   validatePrice,
