953aa39ce073cfb2dd3f3f54e6bc20cc223f8146
Yago Moreira <yagomoreira@gmail.com>
2023-11-08 10:48:43 -0300

    Criando testes para a rota /people com método GET

--- BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-nodeJs-Express-e-MySQL/trybecash-api/tests/integration/people.test.js
+++ BackEnd/secao-04-introducao-ao-desenvolvimento-web-com-nodejs/dia-05-nodeJs-Express-e-MySQL/trybecash-api/tests/integration/people.test.js
@@ -9,6 +9,25 @@ const { expect, use } = chai;
 
 use(chaiHttp);
 
+
+const peopleList = [
+  {
+    id: 1,
+    firstName: 'Luke',
+    lastName: 'Skywalker',
+    email: 'luke.skywalker@trybe.com',
+    phone: '851 678 4453',
+  },
+  {
+    id: 2,
+    firstName: 'Dart',
+    lastName: 'Vader',
+    email: 'dart.vader@trybe.com',
+    phone: '851 678 5665',
+  },
+];
+
+
 describe('Testando os endpoints de people', function () {
   afterEach(() => {
     sinon.restore();
@@ -31,5 +50,27 @@ describe('Testando os endpoints de people', function () {
 
     expect(response.status).to.be.equal(201);
     expect(response.body).to.deep.equal({ message: "Pessoa cadastrada com sucesso com o id 42" });
-  })
-})
+  });
+
+  it('Testando a listagem de todas as pessoas', async function () {
+    sinon.stub(connection, 'execute').resolves([peopleList]);
+
+    const response = await chai
+      .request(app)
+      .get('/people');
+    
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.deep.equal(peopleList);
+  });
+
+  it('Testando a listagem da pessoa com id 1', async function () {
+    sinon.stub(connection, 'execute').resolves([[peopleList[0]]]);
+
+    const response = await chai
+      .request(app)
+      .get('/people/1');
+    
+    expect(response.status).to.be.equal(200);
+    expect(response.body).to.deep.equal(peopleList[0]);
+  });
+});
