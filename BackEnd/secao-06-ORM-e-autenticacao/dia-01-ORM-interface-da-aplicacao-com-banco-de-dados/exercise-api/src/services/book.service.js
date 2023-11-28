aecc42fb83ad0d6eea75fa6e9b6cd1641bc77706
Yago Moreira <yagomoreira@gmail.com>
2023-11-28 20:08:11 -0300

    Exercicio 12 - Book Service - fn updateBook

--- BackEnd/secao-06-ORM-e-autenticacao/dia-01-ORM-interface-da-aplicacao-com-banco-de-dados/exercise-api/src/services/book.service.js
+++ BackEnd/secao-06-ORM-e-autenticacao/dia-01-ORM-interface-da-aplicacao-com-banco-de-dados/exercise-api/src/services/book.service.js
@@ -15,8 +15,18 @@ const createBook = async (title, author, pageQuantity) => {
   return newBook;
 }
 
+const updateBook = async (id, title, author, pageQuantity) => {
+  const [updatedBook] = await Book.update(
+    { title, author, pageQuantity },
+    { where: {id} },
+  );
+
+  return updatedBook;
+}
+
 module.exports = {
   getAll,
   getById,
-  createBook
+  createBook,
+  updateBook
 }
