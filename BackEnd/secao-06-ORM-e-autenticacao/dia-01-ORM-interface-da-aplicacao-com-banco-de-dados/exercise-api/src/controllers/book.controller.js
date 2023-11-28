9661a34df242984810e302968f6924168cf6c2a0
Yago Moreira <yagomoreira@gmail.com>
2023-11-28 20:16:58 -0300

    Exercicio 16 - Book Controller - fn deleteBook

--- BackEnd/secao-06-ORM-e-autenticacao/dia-01-ORM-interface-da-aplicacao-com-banco-de-dados/exercise-api/src/controllers/book.controller.js
+++ BackEnd/secao-06-ORM-e-autenticacao/dia-01-ORM-interface-da-aplicacao-com-banco-de-dados/exercise-api/src/controllers/book.controller.js
@@ -51,10 +51,24 @@ const updateBook = async (req, res) => {
   }
 }
 
+const deleteBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteResponse = await BookService.deleteBook(id);
+    if (!deleteResponse) return res.status(404).json({ message: 'Book not found' });
+
+    return res.status(200).json({ message: 'Book deleted!' });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error500Message });
+  }
+};
+
 
 module.exports = {
   getAll,
   getById,
   createBook,
-  updateBook
+  updateBook,
+  deleteBook,
 }
