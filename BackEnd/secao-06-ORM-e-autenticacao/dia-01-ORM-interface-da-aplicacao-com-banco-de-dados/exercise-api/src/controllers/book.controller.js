12ca0d4217ef1d0fbda6ebfef70a54c3aac57612
Yago Moreira <yagomoreira@gmail.com>
2023-11-28 20:11:52 -0300

    Exercicio 13 - Book Controller - fn updateBook

--- BackEnd/secao-06-ORM-e-autenticacao/dia-01-ORM-interface-da-aplicacao-com-banco-de-dados/exercise-api/src/controllers/book.controller.js
+++ BackEnd/secao-06-ORM-e-autenticacao/dia-01-ORM-interface-da-aplicacao-com-banco-de-dados/exercise-api/src/controllers/book.controller.js
@@ -37,9 +37,24 @@ const createBook = async (req, res) => {
   }
 };
 
+const updateBook = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { title, author, pageQuantity } = req.body;
+    const updatedBook = await BookService.updateBook(id, title, author, pageQuantity);
+    if (!updatedBook) return res.status(404).json({ message: 'Book not found' });
+    
+    return res.status(200).json({ message: "Book updated"});
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error500Message });
+  }
+}
+
 
 module.exports = {
   getAll,
   getById,
-  createBook
+  createBook,
+  updateBook
 }
