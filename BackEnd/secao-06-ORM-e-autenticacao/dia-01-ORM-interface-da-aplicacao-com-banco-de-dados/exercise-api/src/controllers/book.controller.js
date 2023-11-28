c07f8ec1b3f420b9606489822c6e7650a82907ff
Yago Moreira <yagomoreira@gmail.com>
2023-11-28 19:55:50 -0300

    Exercicio 7 - Book Controller - fn getById

--- BackEnd/secao-06-ORM-e-autenticacao/dia-01-ORM-interface-da-aplicacao-com-banco-de-dados/exercise-api/src/controllers/book.controller.js
+++ BackEnd/secao-06-ORM-e-autenticacao/dia-01-ORM-interface-da-aplicacao-com-banco-de-dados/exercise-api/src/controllers/book.controller.js
@@ -1,5 +1,7 @@
 const BookService = require('../services/book.service');
 
+const error500Message = 'Algo deu errado';
+
 const getAll = async (_req, res) => {
   try {
     const books = BookService.getAll();
@@ -8,8 +10,23 @@ const getAll = async (_req, res) => {
     console.log(error.message);
     res.status(500).json({ message: 'Ocorreu um erro' }); 
   }
-}
+};
+
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await BookService.getById(id);
+    if (!book) return res.status(404).json({ message: 'Book not found' });
+
+    return res.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error500Message });
+  }
+};
+
 
 module.exports = {
   getAll,
+  getById,
 }
