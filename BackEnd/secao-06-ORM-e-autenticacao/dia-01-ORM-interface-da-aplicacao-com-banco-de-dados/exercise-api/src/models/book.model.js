54ec991cd22c2ceb108c170e42a7b55474b44dec
Yago Moreira <yagomoreira@gmail.com>
2023-11-28 19:28:34 -0300

    Exercicio 2 - Book Model

---
+++ BackEnd/secao-06-ORM-e-autenticacao/dia-01-ORM-interface-da-aplicacao-com-banco-de-dados/exercise-api/src/models/book.model.js
@@ -0,0 +1,36 @@
+const BookModel = (sequelize, DataTypes) => {
+  const Book = sequelize.define('Book', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    pageQuantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    }
+  }, {
+    tableName: "books"
+  });
+
+  return Book;
+}
+
+module.exports = BookModel;
