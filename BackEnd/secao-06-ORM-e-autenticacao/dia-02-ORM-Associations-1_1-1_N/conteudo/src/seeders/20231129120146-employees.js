2cc7e57ea5b49c640b86932199479aa8eef87e8e
Yago Moreira <yagomoreira@gmail.com>
2023-11-29 09:08:02 -0300

    Employees and Addresses Seeds

---
+++ BackEnd/secao-06-ORM-e-autenticacao/dia-02-ORM-Associations-1_1-1_N/conteudo/src/seeders/20231129120146-employees.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const { query } = require('express');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("employees", 
+    [
+      {
+        first_name: "Marcos",
+        last_name: "Zuck",
+        age: 49,
+      },
+      {
+        first_name: "Fred",
+        last_name: "Mercúrio",
+        age: 19,
+      },
+      {
+        first_name: "Ayrton",
+        last_name: "Keno",
+        age: 51,
+      },
+      {
+        first_name: "Robin",
+        last_name: "Mathias",
+        age: 63,
+      }
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('employees', null, {});
+  },
+};
