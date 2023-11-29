9fbbef03a557204eebb5df947dc3c18a40ba2475
Yago Moreira <yagomoreira@gmail.com>
2023-11-29 09:01:13 -0300

    Address Model e Employee Model. Associando foreign key employeeId

---
+++ BackEnd/secao-06-ORM-e-autenticacao/dia-02-ORM-Associations-1_1-1_N/conteudo/src/models/employee.model.js
@@ -0,0 +1,33 @@
+const EmployeeModel = (sequelize, DataTypes) => {
+  const Employee = sequelize.define('Employee', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+    },
+    age: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    timestamps: false,
+    tableName: 'employees',
+    underscore: true,
+  });
+
+  Employee.associate = (models) => {
+    Employee.hasOne(models.Address, 
+      { foreignKey: 'employeeId', as: 'addresses'});
+  };
+
+  return Employee;
+}
+
+module.exports = EmployeeModel;
