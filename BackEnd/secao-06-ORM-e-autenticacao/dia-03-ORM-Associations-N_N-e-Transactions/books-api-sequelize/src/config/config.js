b7a34cd4d006ca01b24e90bc00373a766b0befb7
Yago Moreira <yagomoreira@gmail.com>
2023-11-30 09:59:35 -0300

    userBook Service and Controller. GET /userbooks/:id

--- BackEnd/secao-06-ORM-e-autenticacao/dia-03-ORM-Associations-N_N-e-Transactions/books-api-sequelize/src/config/config.js
+++ BackEnd/secao-06-ORM-e-autenticacao/dia-03-ORM-Associations-N_N-e-Transactions/books-api-sequelize/src/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: process.env.DATABASE_USER || 'root',
-    password: process.env.DATABASE_PASSWORD || 'password',
+    password: process.env.DATABASE_PASSWORD || 'senha_mysql',
     database: `${process.env.DATABASE_NAME || 'books_database'}_development`,
     host: process.env.DATABASE_HOSTNAME || 'localhost',
     port: process.env.DATABASE_PORT || 3306,
@@ -9,7 +9,7 @@ module.exports = {
   },
   test: {
     username: process.env.DATABASE_USER || 'root',
-    password: process.env.DATABASE_PASSWORD || 'password',
+    password: process.env.DATABASE_PASSWORD || 'senha_mysql',
     database: `${process.env.DATABASE_NAME || 'books_database'}_test`,
     host: process.env.DATABASE_HOSTNAME || 'localhost',
     port: process.env.DATABASE_PORT || 3306,
@@ -17,7 +17,7 @@ module.exports = {
   },
   production: {
     username: process.env.DATABASE_USER || 'root',
-    password: process.env.DATABASE_PASSWORD || 'password',
+    password: process.env.DATABASE_PASSWORD || 'senha_mysql',
     database: `${process.env.DATABASE_NAME || 'books_database'}_production`,
     host: process.env.DATABASE_HOSTNAME || 'localhost',
     port: process.env.DATABASE_PORT || 3306,
