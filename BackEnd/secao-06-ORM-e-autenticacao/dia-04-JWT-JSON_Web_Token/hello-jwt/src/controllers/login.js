94ea61a85cb784473e810a6543cbf72214ec6a6d
Yago Moreira <yagomoreira@gmail.com>
2023-12-06 09:03:21 -0300

    Exercicio 2 - Regra de negocio para conta admin

--- BackEnd/secao-06-ORM-e-autenticacao/dia-04-JWT-JSON_Web_Token/hello-jwt/src/controllers/login.js
+++ BackEnd/secao-06-ORM-e-autenticacao/dia-04-JWT-JSON_Web_Token/hello-jwt/src/controllers/login.js
@@ -13,11 +13,13 @@ module.exports = (req, res) => {
 
   const jwtConfig = { expiresIn: '1h', algorithm: 'HS256' };
 
-  const payload = {
-    username,
-    admin: false
-  };
+  const admin = (username === 'admin' && password === 's3nh4S3gur4???')
 
+  const payload = { 
+    username,
+    admin
+  };
+  
   const token = jwt.sign(payload, secret, jwtConfig);
 
   return res.status(200).json({ token });
