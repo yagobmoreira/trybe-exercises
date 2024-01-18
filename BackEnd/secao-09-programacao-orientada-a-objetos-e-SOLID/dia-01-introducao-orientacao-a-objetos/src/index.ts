06a882e3ac2b09a768a84928ceec9ae6b2e36ee4
Yago Moreira <yagomoreira@gmail.com>
2024-01-18 10:26:55 -0300

    Refatorando a class Person para ilustrar a sintaxe de encapsulamento

--- BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-01-introducao-orientacao-a-objetos/src/index.ts
+++ BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-01-introducao-orientacao-a-objetos/src/index.ts
@@ -1,44 +1,45 @@
 class Person {
   name: string;
-  height: number;
-  weight?: number; // o caractere "?" indica um atributo opcional
+  private _weight: number;
+  private _age: number;
+  readonly height: number; // o caractere "?" indica um atributo opcional
 
-  constructor(n: string, h: number, w?: number) {
-    console.log(`Creating person ${n}`);
-    this.name = n;
-    this.height = h;
-    this.weight = w;
+  constructor(name: string, height: number, weight: number, age: number) {
+    this.name = name;
+    this.height = height;
+    this._weight = weight;
+    this._age = age;
   }
 
-  sleep() {
-    console.log(`${this.name}: zzzzzzz`);
+  getWeight(): number {
+    return this._weight;
+  }
+
+  get age(): number {
+    return this._age;
+  }
+
+  set age(newValue: number) {
+    if (newValue >= 0 && newValue < 200) {
+      this._age = newValue;
+    }
+  }
+
+  birthday(): void {
+    this._age += 1;
   }
 }
 
-const p1 = new Person('Maria', 171, 58);
-// aqui estamos passando somente dois parâmetros, atente-se ao valor do atributo p2.weight
-const p2 = new Person('João', 175);
-console.log(p1.name, p1.height, p1.weight);
-console.log(p2.name, p2.height, p2.weight);
-p1.sleep();
-p2.sleep();
+const p1 = new Person('Maria', 181, 58, 19);
+const p2 = new Person('João', 175, 66, 18);
 
-/*
-Saída:
-Creating person Maria
-Creating person João
-Maria 171 58
-João 175 undefined
-Maria: zzzzzzz
-João: zzzzzzz
-*/
 
 class Tv {
-  brand: string;
-  size: number;
-  resolution: string;
-  connections: string[];
-  connectedTo?: string;
+  public brand: string;
+  public size: number;
+  public resolution: string;
+  public connections: string[];
+  public connectedTo?: string;
 
   constructor(brand: string, size: number, resolution: string, connections: string[]) {
     this.brand = brand;
@@ -48,13 +49,13 @@ class Tv {
   }
 
   turnOn(): void {
-    console.log(`TV ${this.brand}, ${this.size}", resolution: ${this.resolution}
-    connections: ${this.connections}`
-    );
+    console.log(`TV ${this.brand}, ${this.size}", resolution: ${this.resolution}`);
   }
 }
 
 const tv1 = new Tv('LG', 32, '4K', ['HDMI', 'Ethernet', 'Wi-Fi']);
+
+const tv2 = new Tv('Samsung', 42, 'Full HD', ['HDMI', 'Ethernet', 'Wi-Fi']);
 
 tv1.turnOn();
 
