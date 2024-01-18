1b9e936e97b07d716d29028fb1bf7a535dc4f78a
Yago Moreira <yagomoreira@gmail.com>
2024-01-18 10:01:34 -0300

    Exercício para fixar. class Tv

--- BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-01-introducao-orientacao-a-objetos/src/index.ts
+++ BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-01-introducao-orientacao-a-objetos/src/index.ts
@@ -32,3 +32,29 @@ João 175 undefined
 Maria: zzzzzzz
 João: zzzzzzz
 */
+
+class Tv {
+  brand: string;
+  size: number;
+  resolution: string;
+  connections: string[];
+  connectedTo?: string;
+
+  constructor(brand: string, size: number, resolution: string, connections: string[]) {
+    this.brand = brand;
+    this.size = size;
+    this.resolution = resolution;
+    this.connections = connections;
+  }
+
+  turnOn(): void {
+    console.log(`TV ${this.brand}, ${this.size}", resolution: ${this.resolution}
+    connections: ${this.connections}`
+    );
+  }
+}
+
+const tv1 = new Tv('LG', 32, '4K', ['HDMI', 'Ethernet', 'Wi-Fi']);
+
+tv1.turnOn();
+
