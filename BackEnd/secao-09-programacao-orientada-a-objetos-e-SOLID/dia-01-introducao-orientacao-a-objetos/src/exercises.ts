bc3b3d454bd2d409c23485b92f96c3d40d1adba5
Yago Moreira <yagomoreira@gmail.com>
2024-01-18 18:35:38 -0300

    Refatorando exercício 01

--- BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-01-introducao-orientacao-a-objetos/src/exercises.ts
+++ BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-01-introducao-orientacao-a-objetos/src/exercises.ts
@@ -4,21 +4,39 @@ class PessoaEstudante {
   private _notasProvas: number[];
   private _notasTrabalhos: number[];
 
-  constructor (nome: string, matricula: string, notasProvas: number[], notasTrabalhos: number[]) {
+  constructor (nome: string, matricula: string) {
     this._nome = nome;
     this._matricula = matricula;
+  }
+  
+  get matricula(): string { return this._matricula; }
+  
+  set matricula(newMatricula: string) { this._matricula = newMatricula; }
+  
+  get nome(): string { return this._nome; }
 
-    if (notasProvas.length !== 4) {
-      throw new Error('Invalid number of tests');
-    }
-    this._notasProvas = notasProvas;
-
-    if (notasTrabalhos.length !== 2) {
-      throw new Error('Invalid number of assignments');
-    }   
-    this._notasTrabalhos = notasTrabalhos;
+  set nome(newNome: string) { 
+    if (newNome.length < 3) throw new Error('O nome deve conter no mínimo 3 caracteres');
+    this._nome = newNome; 
   }
 
+  get notasProvas(): number[] { return this._notasProvas; }
+  
+  set notasProvas(newNotasProvas: number[]) { 
+    if (newNotasProvas.length > 4) {
+      throw new Error('A pessoa estudante só pode ter 4 notas de prova');
+    }
+    this._notasProvas = newNotasProvas; 
+  }
+
+  get notasTrabalhos(): number[] { return this._notasTrabalhos; }
+
+  set notasTrabalhos(newNotasTrabalhos: number[]) { 
+    if (newNotasTrabalhos.length > 2) {
+      throw new Error('A pessoa estudante só pode ter 2 notas de trabalho');
+    }
+    this._notasTrabalhos = newNotasTrabalhos; 
+  }
   somaNotas(notas: number[]): number {
     return notas.reduce((acc, nota) => acc + nota, 0);
   }
@@ -26,4 +44,6 @@ class PessoaEstudante {
   mediaNotas(notas: number[]): number {
     return this.somaNotas(notas) / notas.length;
   }
+
+
 }
