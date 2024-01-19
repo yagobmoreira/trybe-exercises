4f71e92a8ce19fcf3d312c96295f8b78b10ec0ce
Yago Moreira <yagomoreira@gmail.com>
2024-01-19 14:35:45 -0300

    Herança e Composição

--- BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-02-heranca-e-composicao/conteudo/src/index.ts
+++ BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-02-heranca-e-composicao/conteudo/src/index.ts
@@ -40,3 +40,48 @@ class MyClass implements MyInterface {
 
 const myObj = new MyClass(10);
 console.log(myObj.myFunc(5));
+
+
+// Herança e Composição
+interface Logger {
+  log(param: string): void;
+}
+
+class ConsoleLogger implements Logger {
+  log(param: string) {
+    console.log(param)
+  }
+}
+
+class ConsoleLogger2 implements Logger {
+  log(param: string) {
+    console.log(`Logger2: ${param}`)
+  }
+}
+
+interface Database {
+  logger: Logger;
+  save(key: string, value: string): void;
+}
+
+class ExampleDatabase implements Database {
+  constructor(public logger: Logger = new ConsoleLogger()) { }
+
+  save(key: string, value: string) {
+    this.logger.log(`Salvando o valor ${value} na chave ${key}`)
+  }
+}
+
+// Crie um objeto de cada um dos _Loggers_.
+const logger1 = new ConsoleLogger();
+const logger2 = new ConsoleLogger2();
+
+// Crie três objetos da _ExampleDatabase_, cada um dos dois primeiros recebendo um dos _Loggers_, e o último não recebendo nenhum.
+const database1 = new ExampleDatabase(logger1);
+const database2 = new ExampleDatabase(logger2);
+const database3 = new ExampleDatabase();
+
+// Utilize todos eles para salvar um conteúdo fictício.
+database1.save('chave 1', 'valor 1');
+database2.save('chave 2', 'valor 2');
+database3.save('chave 3', 'valor 3');
