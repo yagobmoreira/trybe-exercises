e689f1a786381e992c6d3a10cbffc529dd5a2422
Yago Moreira <yagomoreira@gmail.com>
2024-01-20 14:07:20 -0300

    Exercicio 05 - class Teacher

--- BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-02-heranca-e-composicao/conteudo/src/index.ts
+++ BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-02-heranca-e-composicao/conteudo/src/index.ts
@@ -1,5 +1,7 @@
 import Person from "./Entities/Person";
 import Student from "./Entities/Student";
+import Subject from "./Entities/Subject";
+import Teacher from "./Entities/Teacher";
 class Superclass {
   constructor(public isSuper: boolean = true) { }
 
@@ -112,3 +114,15 @@ tamires.assignmentsGrades = [47, 42];
 // console.log(jessica);
 // console.log(tamires);
 // console.log(fernando);
+
+const math = new Subject('Matemática');
+const history = new Subject('História');
+const philosophy = new Subject('Filosofia');
+
+const marta = new Teacher('Marta da Silva', new Date('1980/03/30'), 2000, math);
+const joao = new Teacher('João Antônio da Costa', new Date('1982/04/21'), 2000, history);
+const lucio = new Teacher('Lucio Teixeira', new Date('1986/01/29'), 2000, philosophy);
+
+console.log(marta);
+console.log(joao);
+console.log(lucio);
