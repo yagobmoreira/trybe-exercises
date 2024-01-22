18d95b119cf804745e7a98e4191596c7b83214c0
Yago Moreira <yagomoreira@gmail.com>
2024-01-22 14:43:35 -0300

    Para fixar: Métodos e atributos estáticos

--- BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-03-polimorfismo/conteudo/src/Entities/Character.ts
+++ BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-03-polimorfismo/conteudo/src/Entities/Character.ts
@@ -1,4 +1,9 @@
 export default abstract class Character {
   abstract talk(): void;
   abstract specialMove(): void;
+
+  static characterPresentation(character: Character) {
+    character.talk();
+    character.specialMove();
+  }
 }
--- BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-03-polimorfismo/conteudo/src/index.ts
+++ BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-03-polimorfismo/conteudo/src/index.ts
@@ -1,10 +1,14 @@
 import LongRangeCharacter from "./Entities/LongRangeCharacter";
 import MeleeCharacter from "./Entities/MeleeCharacter";
+import Character from "./Entities/Character";
 
 const yoshi = new MeleeCharacter('Yoshi', 'Super dragon');
 const samus = new LongRangeCharacter('Samus', 'Zero Laser');
 
-yoshi.talk();
-yoshi.specialMove();
-samus.talk();
-samus.specialMove();
+// yoshi.talk();
+// yoshi.specialMove();
+// samus.talk();
+// samus.specialMove();
+
+Character.characterPresentation(yoshi);
+Character.characterPresentation(samus);
