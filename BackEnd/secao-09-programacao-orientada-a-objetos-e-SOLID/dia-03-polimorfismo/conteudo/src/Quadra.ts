90f8117bc199571ee575b37690446f77061953dd
Yago Moreira <yagomoreira@gmail.com>
2024-01-23 08:42:58 -0300

    Exercicio 02

--- BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-03-polimorfismo/conteudo/src/Quadra.ts
+++ BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-03-polimorfismo/conteudo/src/Quadra.ts
@@ -1,4 +1,8 @@
+import IAgenda from "./interfaces/IAgenda";
+
 // src/Quadra.ts
-class Quadra { }
+abstract class Quadra {
+  protected abstract reservar<T>(horaReservada: Date): IAgenda<T>
+};
 
 export default Quadra;
