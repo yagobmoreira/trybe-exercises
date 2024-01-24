a3328c9f5b23bcccf46bddfcb429770bd94868af
Yago Moreira <yagomoreira@gmail.com>
2024-01-24 15:07:12 -0300

    Refatorando a Api respeitando os principios SOLID. Adiciona os endpoints GET /plants/:id, PUT /plants/:id, DELETE /plants/:id

---
+++ BackEnd/secao-09-programacao-orientada-a-objetos-e-SOLID/dia-04-SOLID-introducao-e-principios-S_O_D/exercicios/src/models/interfaces/index.ts
@@ -0,0 +1,10 @@
+export { INewPlant, IPlant } from './IPlant';
+
+export {
+  IModel,
+  IModelReader,
+  IModelWriter,
+  IModelDelete,
+} from './IModel';
+
+export { default as IPlantsMetadata } from './IPlantsMetadata';
