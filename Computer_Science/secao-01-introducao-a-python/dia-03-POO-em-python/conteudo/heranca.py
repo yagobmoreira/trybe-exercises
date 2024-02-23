441e5e78615022351369498143452686dff0a021
Yago Moreira <yagomoreira@gmail.com>
2024-02-23 18:31:29 -0300

    add: heranca.py

---
+++ Computer_Science/secao-01-introducao-a-python/dia-03-POO-em-python/conteudo/heranca.py
@@ -0,0 +1,57 @@
+class Eletrodomestico:
+    def __init__(self, cor, potencia, tensao, preco):
+        self.preco = preco
+        self.cor = cor
+        self._potencia = potencia
+        self._tensao = tensao
+        self.__ligado = False
+        self.__velocidade = 0
+        self.__velocidade_maxima = 3
+        self.__corrente_atual_no_motor = 0
+
+    def ligar(self, velocidade):
+        if velocidade > self.__velocidade_maxima or velocidade < 0:
+            raise ValueError(
+                f"Velocidade deve estar entre 0 e {self.__velocidade_maxima}"
+            )
+
+        self.__velocidade = velocidade
+        self.__corrente_atual_no_motor = (
+            (self._potencia / self._tensao) / self.__velocidade_maxima
+        ) * velocidade
+        self.__ligado = True
+
+    def desligar(self):
+        self.__ligado = False
+        self.__velocidade = 0
+
+    def esta_ligado(self):
+        return self.__ligado
+
+    @property
+    def cor(self):
+        return self.__cor.upper()
+
+    @cor.setter
+    def cor(self, nova_cor):
+        self.__cor = nova_cor
+
+
+class Secador(Eletrodomestico):
+    pass
+
+
+class Batedeira(Eletrodomestico):
+    pass
+
+
+class MaquinaDeLavar(Eletrodomestico):
+    pass
+
+
+novo_secador = Secador("Vermelho", 1500, 220, 600)
+print(novo_secador.cor)
+
+nova_batedeira = Batedeira("Preto", 800, 220, 1200)
+nova_batedeira.ligar(3)
+print(nova_batedeira.esta_ligado())
