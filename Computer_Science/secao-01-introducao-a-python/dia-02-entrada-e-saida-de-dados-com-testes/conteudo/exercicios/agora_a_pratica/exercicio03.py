58dfaec31f2c7066105bb887712969465c1b46c8
Yago Moreira <yagomoreira@gmail.com>
2024-02-27 09:39:34 -0300

    fix: exercicio03.py

--- Computer_Science/secao-01-introducao-a-python/dia-02-entrada-e-saida-de-dados-com-testes/conteudo/exercicios/agora_a_pratica/exercicio03.py
+++ Computer_Science/secao-01-introducao-a-python/dia-02-entrada-e-saida-de-dados-com-testes/conteudo/exercicios/agora_a_pratica/exercicio03.py
@@ -2,16 +2,16 @@ import random
 
 
 def choose_word():
-    with open("words.txt", mode="r") as palavras:
+    with open("exercicios/agora_a_pratica/words.txt", mode="r") as palavras:
         words = [word.strip() for word in palavras]
 
     word = random.choice(words)
     scrambled_word = "".join(random.sample(word, len(word)))
     attempts = 1
     while attempts <= 3:
-        print(f"Descubra que palavra é essa: {scrambled_word}")
+        print(f"Descubra que palavra é essa: {scrambled_word.lower()}")
         choosed_word = input("Digite a palavra: ")
-        if choosed_word == word:
+        if choosed_word.lower() == word.lower():
             print("Parabens você acertou!!")
             break
         else:
