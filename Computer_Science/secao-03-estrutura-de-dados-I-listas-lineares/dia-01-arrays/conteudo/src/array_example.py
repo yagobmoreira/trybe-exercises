3f6292efb440bb2a72eece029196487189781c3e
Yago Moreira <yagomoreira@gmail.com>
2024-03-05 14:52:11 -0300

    verificando o que espaço em memoria do array quando é inserido elementos, metodos remove e update

--- Computer_Science/secao-03-estrutura-de-dados-I-listas-lineares/dia-01-arrays/conteudo/src/array_example.py
+++ Computer_Science/secao-03-estrutura-de-dados-I-listas-lineares/dia-01-arrays/conteudo/src/array_example.py
@@ -1,3 +1,6 @@
+import sys
+
+
 class ListaArray:
     def __init__(self) -> None:
         self.data = []
@@ -14,19 +17,38 @@ class ListaArray:
     def set(self, index, value):
         self.data.insert(index, value)
 
+    def remove(self, index):
+        return self.data.pop(index)
+
+    def update(self, index, value):
+        self.data[index] = value
+
 
 if __name__ == "__main__":
     # vamos inicializar e preencher uma estrutura de dados array
     array = ListaArray()
+
+    array_memory_size = sys.getsizeof(array.data)
+    print("Tamanho inicial:", array_memory_size)
+
     array.set(0, "Felipe")
     array.set(1, "Ana")
     array.set(2, "Shirley")
     array.set(3, "Miguel")
 
+    array_memory_size = sys.getsizeof(array.data)
+    print("Tamanho após inserção de itens:", array_memory_size)
+
     # para acessar um elemento do array, utilizamos seu índice
     print(array.get(0))  # saída: Felipe
     print(array.get(2))  # saída: Shirley
     print("-----")
+
+    array.remove(0)
+    print(array)
+
+    array.update(0, "Joaquin")
+    print(array)
 
     # podemos iterar sobre seus elementos da seguinte maneira
     index = 0
