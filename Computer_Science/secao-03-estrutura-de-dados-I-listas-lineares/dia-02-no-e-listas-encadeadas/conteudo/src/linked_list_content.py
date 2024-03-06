f1c1c0f1dfadc14f3c05b9d9edf137706289d942
Yago Moreira <yagomoreira@gmail.com>
2024-03-06 20:38:40 -0300

    Exercicio 01

--- Computer_Science/secao-03-estrutura-de-dados-I-listas-lineares/dia-02-no-e-listas-encadeadas/conteudo/src/linked_list_content.py
+++ Computer_Science/secao-03-estrutura-de-dados-I-listas-lineares/dia-02-no-e-listas-encadeadas/conteudo/src/linked_list_content.py
@@ -22,16 +22,12 @@ class LinkedList(Node):
         self.__length += 1
 
     def insert_last(self, value):
-        last_value = Node(value)
-        current_value = self.head_value
-
         if self.is_empty():
             return self.insert_first(value)
 
-        while current_value.next:
-            current_value = current_value.next
-
-        current_value.next = last_value
+        new_last_value = Node(value)
+        actual_last_value = self.__get_node_at(len(self) - 1)
+        actual_last_value.next = new_last_value
         self.__length += 1
 
     def insert_at(self, value, position):
@@ -39,10 +35,7 @@ class LinkedList(Node):
             return self.insert_first(value)
         if position >= len(self):
             return self.insert_last(value)
-        current_value = self.head_value
-        while position > 1:
-            current_value = current_value.next
-            position -= 1
+        current_value = self.__get_node_at(position - 1)
         next_value = Node(value)
         next_value.next = current_value.next
         current_value.next = next_value
@@ -54,20 +47,18 @@ class LinkedList(Node):
             self.head_value = self.head_value.next
             value_to_be_removed.next = None
             self.__length -= 1
+
         return value_to_be_removed
 
     def remove_last(self):
         if len(self) <= 1:
             return self.remove_first()
 
-        previous_to_be_removed = self.head_value
-
-        while previous_to_be_removed.next.next:
-            previous_to_be_removed = previous_to_be_removed.next
-
+        previous_to_be_removed = self.__get_node_at(len(self) - 2)
         value_to_be_removed = previous_to_be_removed.next
         previous_to_be_removed.next = None
         self.__length -= 1
+
         return value_to_be_removed
 
     def remove_at(self, position):
@@ -76,27 +67,34 @@ class LinkedList(Node):
         if position >= 1:
             return self.remove_last()
 
-        previous_to_be_removed = self.head_value
-        while position > 1:
-            previous_to_be_removed = previous_to_be_removed.next
-            position -= 1
-
+        previous_to_be_removed = self.__get_node_at(position - 1)
         value_to_be_removed = previous_to_be_removed.next
         previous_to_be_removed.next = value_to_be_removed.next
         value_to_be_removed.next = None
         self.__length -= 1
+
         return value_to_be_removed
 
     def get_element_at(self, position):
         value_returned = None
+        value_to_be_returned = self.__get_node_at(position)
+        if value_to_be_returned:
+            value_returned = Node(value_to_be_returned.value)
+
+        return value_returned
+
+    def __get_node_at(self, position):
         value_to_be_returned = self.head_value
         if value_to_be_returned:
             while position > 0 and value_to_be_returned.next:
                 value_to_be_returned = value_to_be_returned.next
                 position -= 1
-            if value_to_be_returned:
-                value_returned = Node(value_to_be_returned.value)
-        return value_returned
+
+        return value_to_be_returned
+
+    def clear(self):
+        while not self.is_empty():
+            self.remove_first()
 
 
 if __name__ == "__main__":
