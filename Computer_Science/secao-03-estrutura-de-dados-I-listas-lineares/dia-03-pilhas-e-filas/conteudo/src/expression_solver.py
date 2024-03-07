64ba094cb0c6aa59b6b03682a48e705d4b2c3864
Yago Moreira <yagomoreira@gmail.com>
2024-03-07 18:34:57 -0300

    Exercicio 06

--- Computer_Science/secao-03-estrutura-de-dados-I-listas-lineares/dia-03-pilhas-e-filas/conteudo/src/expression_solver.py
+++ Computer_Science/secao-03-estrutura-de-dados-I-listas-lineares/dia-03-pilhas-e-filas/conteudo/src/expression_solver.py
@@ -12,6 +12,19 @@ def solve_expression(expr: str):
         elif token == "*":
             result = stack.pop() * stack.pop()
             stack.push(result)
+        elif token == "-":
+            op2 = stack.pop()
+            op1 = stack.pop()
+            result = op1 - op2
+            stack.push(result)
+        elif token == "/":
+            top = stack.pop()
+            next_top = stack.pop()
+            if top == 0:
+                return "Divisão por 0"
+            result = next_top / top
+            stack.push(result)
+
         else:
             stack.push(int(token))
 
@@ -19,3 +32,12 @@ def solve_expression(expr: str):
 
 
 print(solve_expression("5 10 + 3 *"))
+print(solve_expression("5 10 + 30 5 / -"))  # 9
+# B + (A * C) / C * 2 -> 10 5 30 * 30 / 2 * +
+print(solve_expression("10 5 30 * 30 / 2 * +"))  # 20
+# A * B - C + 4 * A - B -> 5 10 * 30 - 4 5 * 10 - +
+print(solve_expression("5 10 * 30 - 4 5 * 10 - +"))  # 30
+# (A + C / B ) * (A + B) -> 30 10 / 5 + 5 10 + *
+print(solve_expression("30 10 / 5 + 5 10 + *"))  # 120
+# 50 * B / 2 * 5 / A -> 50 10 * 2 / 5 * 5 /
+print(solve_expression("50 10 * 2 / 5 * 5 /"))  # 250
