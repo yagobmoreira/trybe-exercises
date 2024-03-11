09b13f347fd4527cc340563da361c11413f9cd38
Yago Moreira <yagomoreira@gmail.com>
2024-03-11 14:06:57 -0300

    add: update_value

--- Computer_Science/secao-04-estrutura-de-dados-II-hashmap-e-set/dia-01-hashmap-e-dict/src/employee.py
+++ Computer_Science/secao-04-estrutura-de-dados-II-hashmap-e-set/dia-01-hashmap-e-dict/src/employee.py
@@ -15,10 +15,30 @@ class Hashmap:
         address = self.get_address(employee.id_num)
         self._buckets[address] = employee
 
-    def get_values(self, id_num):
+    def get_value(self, id_num):
         address = self.get_address(id_num)
         return self._buckets[address].name
 
     def has(self, id_num):
         address = self.get_address(id_num)
         return self._buckets[address] is not None
+
+    def update_value(self, id_num, new_name):
+        address = self.get_address(id_num)
+        employee = self._buckets[address]
+        employee.name = new_name
+
+
+if __name__ == "__main__":
+    employees = [(14, "name1"), (23, "name2"), (10, "name3"), (9, "name4")]
+    registry = Hashmap()
+
+    for id_num, name in employees:
+        employee = Employee(id_num, name)
+        registry.insert(employee)
+
+    print(registry.get_value(23))  # name2
+
+    print(registry.get_value(10))  # name3
+    registry.update_value(10, "name30")
+    print(registry.get_value(10))  # name30
