1827b03b1607a0b971ee57297daedf612e26019e
Yago Moreira <yagomoreira@gmail.com>
2024-03-11 17:53:50 -0300

    colision resolution

--- Computer_Science/secao-04-estrutura-de-dados-II-hashmap-e-set/dia-01-hashmap-e-dict/src/employee.py
+++ Computer_Science/secao-04-estrutura-de-dados-II-hashmap-e-set/dia-01-hashmap-e-dict/src/employee.py
@@ -6,27 +6,34 @@ class Employee:
 
 class Hashmap:
     def __init__(self) -> None:
-        self._buckets = [None for i in range(10)]
+        self._buckets = [[] for i in range(10)]
 
     def get_address(self, id_num):
         return id_num % 10
 
     def insert(self, employee):
         address = self.get_address(employee.id_num)
-        self._buckets[address] = employee
+        self._buckets[address].append(employee)
 
     def get_value(self, id_num):
         address = self.get_address(id_num)
-        return self._buckets[address].name
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                return item.name
+        return None
 
     def has(self, id_num):
         address = self.get_address(id_num)
-        return self._buckets[address] is not None
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                return True
+        return False
 
     def update_value(self, id_num, new_name):
         address = self.get_address(id_num)
-        employee = self._buckets[address]
-        employee.name = new_name
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                item.name = new_name
 
 
 if __name__ == "__main__":
