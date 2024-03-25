0029be3a389ba99cf1be2de6c756c0f32d6e9d44
Yago Moreira <yagomoreira@gmail.com>
2024-03-25 16:15:58 -0300

    Organizando classes em pacotes

--- Java/secao-01-fundamentos-de-java/dia-02-classes-e-tipos/conteudo-do-dia/src/com/betrybe/helloworld/HelloWorld.java
+++ Java/secao-01-fundamentos-de-java/dia-02-classes-e-tipos/conteudo-do-dia/src/com/betrybe/helloworld/HelloWorld.java
@@ -1,3 +1,5 @@
+package com.betrybe.helloworld;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Primeira linha");
---
+++ Java/secao-01-fundamentos-de-java/dia-02-classes-e-tipos/conteudo-do-dia/src/com/betrybe/helloworld/RaizQuadradaDeVinteECinco.java
@@ -0,0 +1,11 @@
+package com.betrybe.helloworld;
+
+import java.lang.*;
+import java.lang.Math;
+
+public class RaizQuadradaDeVinteECinco {
+    public static void main(String[] args) {
+        System.out.println("A raíz quadrada de vinte e cinco é:");
+        System.out.println(Math.sqrt(25));
+    }
+}
