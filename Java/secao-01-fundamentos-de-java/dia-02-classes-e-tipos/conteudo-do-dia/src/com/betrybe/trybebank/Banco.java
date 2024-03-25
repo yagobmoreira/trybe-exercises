c4bcb156ca427d95859d2baf81edbcaf73dfd96e
Yago Moreira <yagomoreira@gmail.com>
2024-03-25 17:20:09 -0300

    Atributos, métodos e construtor

--- Java/secao-01-fundamentos-de-java/dia-02-classes-e-tipos/conteudo-do-dia/src/com/betrybe/trybebank/Banco.java
+++ Java/secao-01-fundamentos-de-java/dia-02-classes-e-tipos/conteudo-do-dia/src/com/betrybe/trybebank/Banco.java
@@ -2,6 +2,11 @@ package com.betrybe.trybebank;
 
 public class Banco {
     public static void main(String[] args) {
-        ContaBancaria conta = new ContaBancaria();
+        ContaBancaria conta = new ContaBancaria(1000);
+
+        conta.mostrarMensagem();
+        conta.depositar(500);
+        double valorConvertido = conta.converteMoedas(30, 5);
+        System.out.println(valorConvertido);
     }
 }
--- Java/secao-01-fundamentos-de-java/dia-02-classes-e-tipos/conteudo-do-dia/src/com/betrybe/trybebank/ContaBancaria.java
+++ Java/secao-01-fundamentos-de-java/dia-02-classes-e-tipos/conteudo-do-dia/src/com/betrybe/trybebank/ContaBancaria.java
@@ -1,4 +1,21 @@
 package com.betrybe.trybebank;
 
 public class ContaBancaria {
+    double saldo;
+
+    public ContaBancaria(double saldo) { //Constructor
+        this.saldo = saldo;
+    }
+
+    void mostrarMensagem() {
+        System.out.println("Conta bancária acessada.");
+    }
+
+    void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    double converteMoedas(float total, float cambio) {
+        return total*cambio;
+    }
 }
