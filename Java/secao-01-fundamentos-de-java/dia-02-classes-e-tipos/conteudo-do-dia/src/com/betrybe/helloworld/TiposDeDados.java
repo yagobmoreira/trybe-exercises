221d44a7f21096c9c9a19c777b9e37649a2a6a1f
Yago Moreira <yagomoreira@gmail.com>
2024-03-25 16:28:21 -0300

    Variaveis e tipos de dados

---
+++ Java/secao-01-fundamentos-de-java/dia-02-classes-e-tipos/conteudo-do-dia/src/com/betrybe/helloworld/TiposDeDados.java
@@ -0,0 +1,10 @@
+package com.betrybe.helloworld;
+
+public class TiposDeDados {
+    public static void main(String[] args) {
+        int respostaParaTudo = 42;
+
+        System.out.println("A resposta para tudo é:");
+        System.out.println(respostaParaTudo);
+    }
+}
