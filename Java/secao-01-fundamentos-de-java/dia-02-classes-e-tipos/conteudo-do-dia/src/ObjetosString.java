09de5a274ec3aa4a85961bd7909bcc789c5fe3fb
Yago Moreira <yagomoreira@gmail.com>
2024-03-25 17:46:12 -0300

    String e Array

---
+++ Java/secao-01-fundamentos-de-java/dia-02-classes-e-tipos/conteudo-do-dia/src/ObjetosString.java
@@ -0,0 +1,7 @@
+public class ObjetosString {
+    public static void main(String[] args) {
+        String frase = "Estou aprendendo Java.";
+        String numeros = "09890";
+        String caractere = "a";
+    }
+}
