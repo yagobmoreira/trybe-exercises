51484c7bb0d6f51116ad9613f9b9dc5bffff59e8
Yago Moreira <yagomoreira@gmail.com>
2024-03-26 13:26:57 -0300

    Estruturas condicionais

---
+++ Java/secao-01-fundamentos-de-java/dia-03-controle-de-fluxo-e-entrada-de-dados/conteudo/src/CondicionalIf.java
@@ -0,0 +1,15 @@
+public class CondicionalIf {
+    public static void main(String[] args) {
+        double mediaFinal = 6.0;
+        String resultado = "Indefinido";
+
+        if (mediaFinal >= 7) {
+            resultado = "Aprovado";
+        } else if (mediaFinal >= 6.0) {
+            resultado = "Realizar prova bônus";
+        } else {
+            resultado = "Reprovado";
+        }
+        System.out.println(resultado);
+    }
+}
