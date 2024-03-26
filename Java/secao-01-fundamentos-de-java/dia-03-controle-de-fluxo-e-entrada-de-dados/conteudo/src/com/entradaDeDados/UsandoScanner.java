f3b7a69574eb0fbff452823da8f4966596d7a919
Yago Moreira <yagomoreira@gmail.com>
2024-03-26 14:38:09 -0300

    Conversão de tipos

--- Java/secao-01-fundamentos-de-java/dia-03-controle-de-fluxo-e-entrada-de-dados/conteudo/src/com/entradaDeDados/UsandoScanner.java
+++ Java/secao-01-fundamentos-de-java/dia-03-controle-de-fluxo-e-entrada-de-dados/conteudo/src/com/entradaDeDados/UsandoScanner.java
@@ -7,7 +7,24 @@ public class UsandoScanner {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Qual o seu nome: ");
         String nome = scanner.next();
+        System.out.print("Entre com o primeiro número: ");
+        String input1 = scanner.next();
+        System.out.print("Entre com o seguindo número: ");
+        String input2 = scanner.next();
+        System.out.print("Entre com o número: ");
+
+        String input = scanner.next();
+        short numeroInteiroPequeno = Short.parseShort(input); // Converte para short.
+        long numeroInteiroBemGrande = Long.parseLong(input); // Converte para long.
+        float numeroDePontoFlutuante = Float.parseFloat(input); // Converte para float.
+        double numeroDePontoFlutuanteComPrecisaoDupla = Double.parseDouble(input); // Converte para double.
+
+        int n1 = Integer.parseInt(input1);
+        int n2 = Integer.parseInt(input2);
+        int resultado = n1 + n2;
         System.out.println("Boas-vindas, " + nome + "!");
+        System.out.println("O resultado da soma é: " + resultado);
+
         scanner.close();
     }
 }
