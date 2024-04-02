147920e48d4f6655caf1d74ae121f056923c593d
Yago Moreira <yagomoreira@gmail.com>
2024-04-02 17:04:30 -0300

    Atributos

--- Java/secao-02-orientacao-a-objetos/dia-01-abstracao-e-encapsulamento/orientacao-a-objetos/src/main/java/com/betrybe/electronic/Application.java
+++ Java/secao-02-orientacao-a-objetos/dia-01-abstracao-e-encapsulamento/orientacao-a-objetos/src/main/java/com/betrybe/electronic/Application.java
@@ -1,11 +1,19 @@
 package com.betrybe.electronic;
 
+/**
+ * The type Application.
+ */
 public class Application {
 
+  /**
+   * The entry point of application.
+   *
+   * @param args the input arguments
+   */
   public static void main(String[] args) {
     System.out.println("Iniciando o sistema...");
 
-    Television television = new Television();
+    Television television = new Television("Phillips", "Smart TV", 55);
 
     System.out.println("Finalizando o sistema...");
   }
--- Java/secao-02-orientacao-a-objetos/dia-01-abstracao-e-encapsulamento/orientacao-a-objetos/src/main/java/com/betrybe/electronic/Television.java
+++ Java/secao-02-orientacao-a-objetos/dia-01-abstracao-e-encapsulamento/orientacao-a-objetos/src/main/java/com/betrybe/electronic/Television.java
@@ -1,8 +1,35 @@
 package com.betrybe.electronic;
 
+/**
+ * The type Television.
+ */
 public class Television {
-  public Television() {
+
+  /**
+   * The Is on.
+   */
+  boolean isOn = false;
+  String brand;
+  /**
+   * The Model.
+   */
+  String model;
+  /**
+   * The Size.
+   */
+  int size;
+
+  /**
+   * Instantiates a new Television.
+   *
+   * @param brand the brand
+   * @param model the model
+   * @param size  the size
+   */
+  public Television(String brand, String model, int size) {
+    this.brand = brand;
+    this.model = model;
+    this.size = size;
     System.out.println("Construtor da classe Television sendo executado...");
   }
-
 }
