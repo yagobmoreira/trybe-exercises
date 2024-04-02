e1621bd9b94a5895e431b76caad0220a0a532b85
Yago Moreira <yagomoreira@gmail.com>
2024-04-02 16:25:19 -0300

    Construtores

---
+++ Java/secao-02-orientacao-a-objetos/dia-01-abstracao-e-encapsulamento/orientacao-a-objetos/src/main/java/com/betrybe/electronic/Application.java
@@ -0,0 +1,12 @@
+package com.betrybe.electronic;
+
+public class Application {
+
+  public static void main(String[] args) {
+    System.out.println("Iniciando o sistema...");
+
+    Television television = new Television();
+
+    System.out.println("Finalizando o sistema...");
+  }
+}
---
+++ Java/secao-02-orientacao-a-objetos/dia-01-abstracao-e-encapsulamento/orientacao-a-objetos/src/main/java/com/betrybe/electronic/Television.java
@@ -0,0 +1,8 @@
+package com.betrybe.electronic;
+
+public class Television {
+  public Television() {
+    System.out.println("Construtor da classe Television sendo executado...");
+  }
+
+}
