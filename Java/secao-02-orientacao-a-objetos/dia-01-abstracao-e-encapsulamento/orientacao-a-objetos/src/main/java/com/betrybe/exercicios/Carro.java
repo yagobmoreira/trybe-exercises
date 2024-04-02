169619350db22a3dc2dab0f8fce8dbf7a2265819
Yago Moreira <yagomoreira@gmail.com>
2024-04-02 18:04:22 -0300

    Exercicio 02

--- Java/secao-02-orientacao-a-objetos/dia-01-abstracao-e-encapsulamento/orientacao-a-objetos/src/main/java/com/betrybe/exercicios/Carro.java
+++ Java/secao-02-orientacao-a-objetos/dia-01-abstracao-e-encapsulamento/orientacao-a-objetos/src/main/java/com/betrybe/exercicios/Carro.java
@@ -1,9 +1,9 @@
 package com.betrybe.exercicios;
 
 public class Carro {
-  String marca;
-  String modelo;
-  int ano;
+  private String marca;
+  private String modelo;
+  private int ano;
   public Carro(String marca, String modelo, int ano) {
     this.marca = marca;
     this.modelo = modelo;
@@ -12,5 +12,29 @@ public class Carro {
 
   public String info() {
     return "Marca: %s, Modelo: %s, Ano: %d".formatted(marca, modelo, ano);
+  }
+
+  public String getMarca() {
+    return marca;
+  }
+
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
+
+  public String getModelo() {
+    return modelo;
+  }
+
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
+  }
+
+  public int getAno() {
+    return ano;
+  }
+
+  public void setAno(int ano) {
+    this.ano = ano;
   }
 }
