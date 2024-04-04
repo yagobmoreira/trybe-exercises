0268ff45e4f669d93d6c0de9ba38bb732708b506
Yago Moreira <yagomoreira@gmail.com>
2024-04-04 09:40:28 -0300

    Classes abstratas e polimorfismo

--- Java/secao-02-orientacao-a-objetos/dia-03-classes-abstratas-e-interfaces/tech/src/main/java/com/betrybe/tech/Application.java
+++ Java/secao-02-orientacao-a-objetos/dia-03-classes-abstratas-e-interfaces/tech/src/main/java/com/betrybe/tech/Application.java
@@ -4,6 +4,13 @@ public class Application {
 
   public static void main(String[] args) {
     Laptop laptop = new Laptop("Samsung", 100, 17);
-    laptop.bootUp();
+    Desktop desktop = new Desktop("Dell", 500);
+    checkDevice(laptop);
+    checkDevice(desktop);
+  }
+
+  public static void checkDevice(ComputerDevice device) {
+    System.out.println("Verificando dispositivo de marca " + device.getBrand());
+    device.bootUp();
   }
 }
---
+++ Java/secao-02-orientacao-a-objetos/dia-03-classes-abstratas-e-interfaces/tech/src/main/java/com/betrybe/tech/Desktop.java
@@ -0,0 +1,12 @@
+package com.betrybe.tech;
+
+public class Desktop extends ComputerDevice {
+  public Desktop(String brand, double storageCapacity) {
+    super(brand, storageCapacity);
+  }
+
+  @Override
+  public void bootUp() {
+    System.out.println("Inicializando o computador desktop...");
+  }
+}
