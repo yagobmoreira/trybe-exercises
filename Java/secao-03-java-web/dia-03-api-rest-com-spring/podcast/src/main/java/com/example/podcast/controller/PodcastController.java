eeaa06ad0f210e17f7095c34aec6d60fc6ae53b1
Yago Moreira <yagomoreira@gmail.com>
2024-04-10 10:54:23 -0300

    Parametrizando Rotas GET

--- Java/secao-03-java-web/dia-03-api-rest-com-spring/podcast/src/main/java/com/example/podcast/controller/PodcastController.java
+++ Java/secao-03-java-web/dia-03-api-rest-com-spring/podcast/src/main/java/com/example/podcast/controller/PodcastController.java
@@ -1,7 +1,10 @@
 package com.example.podcast.controller;
 
+import jakarta.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +14,15 @@ public class PodcastController {
   @GetMapping
   public String getRoot() {
     return "Yay, podcasts!";
+  }
+
+  @GetMapping("/{id}")
+  public String getPodcast(@PathVariable Long id) {
+    return String.format("Você pediu pelo Podcast com id: %d", id);
+  }
+
+  @GetMapping("/search")
+  public String searchPodcast(@RequestParam String title) {
+    return String.format("Você buscou por Podcasts com título: %s", title);
   }
 }
