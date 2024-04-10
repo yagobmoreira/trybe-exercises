67a082ad24f34e3a28b1c870c36ae91269b737c0
Yago Moreira <yagomoreira@gmail.com>
2024-04-10 12:00:17 -0300

    Final

--- Java/secao-03-java-web/dia-03-api-rest-com-spring/podcast/src/main/java/com/example/podcast/controller/PodcastController.java
+++ Java/secao-03-java-web/dia-03-api-rest-com-spring/podcast/src/main/java/com/example/podcast/controller/PodcastController.java
@@ -6,7 +6,6 @@ import com.example.podcast.model.PodcastDto;
 import com.example.podcast.service.PodCastService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
