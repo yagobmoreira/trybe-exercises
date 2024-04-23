21b0e3b980bb50a7269aaf529706a38232f5d9e0
Yago Moreira <yagomoreira@gmail.com>
2024-04-23 18:07:38 -0300

    Exercício 02

--- Java/secao-04-persistencia-I/dia-01-collections/collections/src/main/java/exercicios/ExerciciosStream.java
+++ Java/secao-04-persistencia-I/dia-01-collections/collections/src/main/java/exercicios/ExerciciosStream.java
@@ -9,9 +9,18 @@ public class ExerciciosStream {
     List<Integer> numerosPares = filtrarNumerosPares(numeros);
 
     System.out.printf(numerosPares.toString());
+
+    List<String> palavras = List.of("java", "stream", "programação", "backend");
+    List<String> palavrasEmCaixaAlta = converterParaCaixaAlta(palavras);
+
+    System.out.printf(palavrasEmCaixaAlta.toString());
   }
 
-  public static List<Integer> filtrarNumerosPares (List<Integer> numeros) {
+  public static List<Integer> filtrarNumerosPares(List<Integer> numeros) {
     return numeros.stream().filter(numero -> numero % 2 == 0).toList();
+  }
+
+  public static List<String> converterParaCaixaAlta(List<String> palavras) {
+    return palavras.stream().map(String::toUpperCase).toList();
   }
 }
