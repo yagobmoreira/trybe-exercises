d41fad41e0c1ca6d75beccbed33d2dd31b19a21b
Yago Moreira <yagomoreira@gmail.com>
2024-04-24 15:17:59 -0300

    add: Controller and Advice

---
+++ Java/secao-04-persistencia-I/dia-02-spring-data-JPA/alexandria/src/main/java/com/betrybe/alexandria/controller/dto/AuthorCreationDto.java
@@ -0,0 +1,9 @@
+package com.betrybe.alexandria.controller.dto;
+
+import com.betrybe.alexandria.entity.Author;
+
+public record AuthorCreationDto(String name, String nationality) {
+  public Author toEntity() {
+    return new Author(name, nationality);
+  }
+}
