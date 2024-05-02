4fe13666d91e862eee0f639ee75daf0237f3754b
Yago Moreira <yagomoreira@gmail.com>
2024-05-02 14:56:50 -0300

    test: ProductTest

---
+++ Java/secao-05-persistencia-II/dia-01-testes-com-springdata/ecommerce/src/test/java/com/betrybe/ecommerce/ProductTest.java
@@ -0,0 +1,29 @@
+package com.betrybe.ecommerce;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.betrybe.ecommerce.entity.Product;
+import com.betrybe.ecommerce.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductTest {
+  @Autowired
+  ProductRepository productRepository;
+
+  @Test
+  public void testProductCreation() {
+    Product product = new Product();
+    product.setName("Panela");
+    product.setPrice(60.35);
+
+    Product savedProduct = productRepository.save(product);
+
+    assertNotNull(savedProduct.getId());
+    assertEquals(product.getName(), savedProduct.getName());
+    assertEquals(product.getPrice(), savedProduct.getPrice());
+  }
+}
