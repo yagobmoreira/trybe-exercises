aa97f3cc09009c6a86d7a9bcb90c2489128bae69
Yago Moreira <yagomoreira@gmail.com>
2024-05-02 15:45:04 -0300

    Spring profiles

--- Java/secao-05-persistencia-II/dia-01-testes-com-springdata/ecommerce/src/test/java/com/betrybe/ecommerce/ProductServiceTest.java
+++ Java/secao-05-persistencia-II/dia-01-testes-com-springdata/ecommerce/src/test/java/com/betrybe/ecommerce/ProductServiceTest.java
@@ -15,8 +15,10 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class ProductServiceTest {
   @Autowired
   ProductService productService;
--- Java/secao-05-persistencia-II/dia-01-testes-com-springdata/ecommerce/src/test/java/com/betrybe/ecommerce/ProductTest.java
+++ Java/secao-05-persistencia-II/dia-01-testes-com-springdata/ecommerce/src/test/java/com/betrybe/ecommerce/ProductTest.java
@@ -8,8 +8,10 @@ import com.betrybe.ecommerce.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class ProductTest {
   @Autowired
   ProductRepository productRepository;
