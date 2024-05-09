07b3b4455c7317dbbad33dc5511bcc153f3c9526
Yago Moreira <yagomoreira@gmail.com>
2024-05-09 20:29:15 -0300

    Protegendo as rotas

--- Java/secao-06-spring-security/dia-01-autenticacao/java-trybe-webinars-main/src/main/java/com/betrybe/webinar/security/SecurityConfig.java
+++ Java/secao-06-spring-security/dia-01-autenticacao/java-trybe-webinars-main/src/main/java/com/betrybe/webinar/security/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.betrybe.webinar.security;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,6 +27,14 @@ public class SecurityConfig {
         .sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         )
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(HttpMethod.POST, "/persons").permitAll()
+            .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+            .requestMatchers(toH2Console()).permitAll()
+            .anyRequest().authenticated()
+        )
+        .headers(headers -> headers
+            .frameOptions(FrameOptionsConfig::sameOrigin))
         .build();
   }
 
